df9d433c3c85d99bcc43c1100ff85d43c6df7098
SAMAEL NEWGATE <SAMNEWGA@uat.edu>
2019-04-22 19:43:33 -0700

    final complete v1

--- SC1.py
+++ SC1.py
@@ -1,6 +1,4 @@
 import pandas as pd
-# import warnings filter
-# Suppresses warnings to user of any dated functions in libs I am using.
 from warnings import simplefilter
 from sklearn.model_selection import train_test_split
 from sklearn import tree
@@ -9,114 +7,115 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
-
-# Suppresses warnings to user of any dated functions in libs I am using.
-# warnings filter
+# Using simplefilter from warnings for warning control
 simplefilter(action='ignore', category=FutureWarning)
 print()
-# intake data
-train = pd.read_excel("ufcfightstats30int.xlsx")
-train.head()
 
-# Create list of
-# Features our model will learn from
-print("_" * 175)
-print("By using the following data in columns")
-print("'PassionForTech', 'Support', 'Expression', 'Desire', 'Score', 'Aptitude'")
-print("We were able to predict if a student would Enroll with a positive outcome the following percentages:")
-feature_cols = ['r_strikes', 'b_strikes', 'r_sigstrikes', 'b_sigstrikes', 'r_takedowns', 'b_takedowns',
+# Using xlsx format data sheet for training
+training = pd.read_excel("ufcfightstats30int.xlsx")
+
+# Head gives us the first five rows from the data frame
+training.head()
+
+
+print("************WELCOME TO UFC FIGHT PREDICTOR************")
+print('-'*100)
+
+print("Program Description: \nThis program uses four different classifiers to predict who was the winner from multiple fights. "
+      "\nThe classifiers use a data frame that has all the stat outcomes from specific recent UFC fights. ")
+
+print("\nClassifiers used:"
+      "\n- Decision Tree Classifier"
+      "\n- Logistic Regression Classifier"
+      "\n- KNeighbors Classifier"
+      "\n- Multilayer Perceptron Classifier")
+
+print("\nStats used: "
+      "\n- Strikes"
+      "\n- Significant strike"
+      "\n- Takedowns"
+      "\n- Knockdowns"
+      "\n- Submissions attempts"
+      "\n- Guard passes"
+      "\n- Reversals"
+      "\n- Win methods"
+      "\n- Last 3 fight outcomes")
+
+print("\nDataframe legend:"
+      "\n- Win method: 1 = KO/TKO, 2 = Submissions, 3 = Decision"
+      "\n- Winner: 1 = Red fighter, 2 = Blue fighter"
+      "\n- Last fight: 1 = Win, 2 = Loss, 3 = Draw")
+
+print('-'*100)
+
+wait = input("\nPress enter to view dataframe example.")
+
+print('-'*100)
+
+# Selecting what our feature columns will be, focusing on non categorical values
+training_features = ['r_strikes', 'b_strikes', 'r_sigstrikes', 'b_sigstrikes', 'r_takedowns', 'b_takedowns',
                 'r_knockdowns', 'b_knockdowns', 'r_subtemps', 'b_subtemps', 'r_pass', 'b_pass', 'r_rev',
                 'b_rev', 'win_method', 'r_last1', 'b_last1', 'r_last2', 'b_last2', 'r_last3', "b_last3"]
 
-# Features X
-X = train.loc[:, feature_cols]
+
+# Using the location function to get particular labels from the index and setting it as X
+X = training.loc[:, training_features]
+
+# Printing out X
 print(X)
-print()
-X = list(map(int, X))
 
-# Loop through list
-# Print list out while looping
-# Now put if statements inside loop
-# if x = 'blue' then x = 1
+# Y is set to training to predict winner
+y = training.winner
 
-print(X)
-# SciKit Learn understand the shape as long as they are fully numeric
-# and the right shape.
-
-# Print out the shape of the data
-# This returns the # of rows by # of columns
-print()
-# print("Verify the shape of the data")
-# print("This returns the # of rows by # of columns")
-# print(X.shape)
-# print(X.reindex)
-
-# Label for prediction
-# using y for labels
-
-y = train.winner
-# print(y.shape)
-# print(y.reindex)
-# Build the scikit Model
-# LogisticRegression Model
-# Build the classification model
-# Import the lib LogisticRegression which is a classification model
-# Create an instance of our model
-# Fit model to training data
-# Split up the data into training set and a testing set
-# Critical to set data from pandas to data frame which
-# SciKit can work with.
-# df = pd.DataFrame(train)
-# print(df)
-
+# Defining our testing split and testing size, testing size is set to 50%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
 
-# Start classifier specific code
+# Using four different types of classifiers to predict the winner of the UFC fight
 
-dt_classifier = tree.DecisionTreeClassifier()
+print('-'*100)
 
-
-
-kn_classifier = KNeighborsClassifier()
-
-
-
-logreg_classifier = LogisticRegression()
-
-
-ANN_classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-
-# End classifier specific code
-
-# Train the classifier using training data.
-dt_classifier.fit(X_train, y_train)
-kn_classifier.fit(X_train, y_train)
-logreg_classifier.fit(X_train, y_train)
-ANN_classifier.fit(X_train, y_train)
-# Use it to classify our test data.
-dt_predictions = dt_classifier.predict(X_test)
-kn_predictions = kn_classifier.predict(X_test)
-logreg_predictions = logreg_classifier.predict(X_test)
-ANN_predictions = ANN_classifier.predict(X_test)
-
-# Actual predictions on our test data
-# 1 = Student Enrolled Positive Outcome is a yes
-# 0 = Student NOT Enrolled Positive Outcome is a no
-# Too many to print out, so it prints abbreviated
-# print(dt_predictions)
-# Determine here how accurate our classifier was on the testing set
-# This compares the predictive labels to the true labels aka y_test
-# Then calculate the accuracy score
-
-print("The accuracy score for the DT Classifier is:")
-print(accuracy_score(y_test, dt_predictions))
-print("The accuracy score for the KN Classifier is:")
-print(accuracy_score(y_test, kn_predictions))
-print("The accuracy score for the LR Classifier is:")
-print(accuracy_score(y_test, logreg_predictions))
-print("The accuracy score for the ANN Classifier is:")
-print(accuracy_score(y_test, ANN_predictions))
+wait = input("\nPress enter to view Decision Tree Classifier accuracy results.")
 
 print()
-print("*" * 25, " End of Line ", "*" * 25)
-print()
+print('-'*100)
+# We call our first classifier, the decision tree classifier from sklearn and display it's accuracy percentage
+DecisionTree_Classifier = tree.DecisionTreeClassifier()
+DecisionTree_Classifier.fit(X_train, y_train)
+DecisiionTree_Prediction = DecisionTree_Classifier.predict(X_test)
+print(accuracy_score(y_test, DecisiionTree_Prediction))
+
+print('-'*100)
+
+wait = input("\nPress enter to view Logistic Regression Classifier accuracy results.")
+
+print('-'*100)
+# We call our second classifier, the logistic regression classifier from sklearn and display it's accuracy percentage
+LogisticRegression_Classifier = LogisticRegression()
+LogisticRegression_Classifier.fit(X_train, y_train)
+LogisticRegression_Prediction = LogisticRegression_Classifier.predict(X_test)
+print(accuracy_score(y_test, LogisticRegression_Prediction))
+
+print('-'*100)
+
+wait = input("\nPress enter to view KNeighbors Classifier accuracy results.")
+
+print('-'*100)
+# We call our third classifier, the KNeighbors classifier from sklearn and display it's accuracy percentage
+KNeighbors_Classifier = KNeighborsClassifier()
+KNeighbors_Classifier.fit(X_train, y_train)
+KNeighbors_Prediction = KNeighbors_Classifier.predict(X_test)
+print(accuracy_score(y_test, KNeighbors_Prediction))
+
+print('-'*100)
+
+wait = input("\nPress enter to view Multilayer Perceptron Classifier accuracy results.")
+
+print('-'*100)
+
+# We then call our fourth and final classifier, the ANN classifier from
+MLP_CLassifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+MLP_CLassifier.fit(X_train, y_train)
+MLP_Prediction = MLP_CLassifier.predict(X_test)
+print(accuracy_score(y_test, MLP_Prediction))
+
+print('-'*100)
